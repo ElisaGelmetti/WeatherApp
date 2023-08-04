98e17812f040d3813794341e1f96d32fa5f7fb58
ElisaGelmetti <134309337+ElisaGelmetti@users.noreply.github.com>
2023-08-04 14:34:31 +0200

    salvo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'WeatherApp';
+  title: any;
+  constructor(private AuthService: AuthService) {}
+
+  isAuthenticated(): boolean {
+    return this.AuthService.isAuthenticated();
+  }
 }
---
+++ src/app/weather-list/weather-list.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { WeatherService } from '../weather.service';
+
+@Component({
+  selector: 'app-weather-list',
+  templateUrl: './weather-list.component.html',
+  styleUrls: ['./weather-list.component.scss'],
+})
+export class WeatherListComponent {
+  cityInfos: any[] = [];
+
+  constructor(private weatherService: WeatherService) {}
+
+  onSearch(cityInfo: string) {
+    const [cityName, stateCode, countryCode] = cityInfo.split(',');
+    this.weatherService
+      .getWeatherByCityName(
+        cityName.trim(),
+        stateCode.trim(),
+        countryCode.trim()
+      )
+      .subscribe(
+        (data: any) => {
+          this.cityInfos.push({
+            cityName: cityName.trim(),
+            weatherData: data.list[0],
+          });
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+  }
+}
